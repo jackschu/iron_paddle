e4fa0ddc1c245cd379197df8720540f365d5334c
jackschu <js9pa@virginia.edu>
2023-11-12 15:08:19 -0500

    paddle follows cursor

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,97 @@
+use bevy::window::PrimaryWindow;
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+/// We will store the world position of the mouse cursor here.
+#[derive(Resource, Default)]
+struct MyWorldCoords(Vec2);
+
+/// Used to help identify our main camera
+#[derive(Component)]
+struct MainCamera;
 
 fn main() {
     println!("hi");
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, my_cursor_system)
+        .add_systems(Update, paddle_movement)
         .run();
 }
+
+#[derive(Component)]
+struct Paddle;
+
+#[derive(Component)]
+struct Ball;
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    commands.init_resource::<MyWorldCoords>();
+    commands.spawn((Camera2dBundle::default(), MainCamera));
 
     // Circle
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
-        ..default()
-    });
-
-    // Rectangle
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgba(0.25, 0.25, 0.75, 0.50),
-            custom_size: Some(Vec2::new(50.0, 100.0)),
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
-        ..default()
-    });
+        Ball,
+    ));
 
-    // Quad
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes
-            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
-            .into(),
-        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
-        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
-        ..default()
-    });
+    // Rectangle
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgba(0.25, 0.25, 0.75, 0.50),
+                custom_size: Some(Vec2::new(50.0, 100.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+            ..default()
+        },
+        Paddle,
+    ));
+}
 
-    // Hexagon
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
-        transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
-        ..default()
-    });
+fn my_cursor_system(
+    mut mycoords: ResMut<MyWorldCoords>,
+    // query to get the window (so we can read the current cursor position)
+    q_window: Query<&Window, With<PrimaryWindow>>,
+    // query to get camera transform
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) {
+    // get the camera info and transform
+    // assuming there is exactly one main camera entity, so Query::single() is OK
+    let (camera, camera_transform) = q_camera.single();
+
+    // There is only one primary window, so we can similarly get it from the query:
+    let window = q_window.single();
+
+    // check if the cursor is inside the window and get its position
+    // then, ask bevy to convert into world coordinates, and truncate to discard Z
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        mycoords.0 = world_position;
+    }
+}
+
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn paddle_movement(
+    mycoords: ResMut<MyWorldCoords>,
+    mut sprite_position: Query<(&mut Paddle, &mut Transform)>,
+) {
+    for (_paddle, mut transform) in &mut sprite_position {
+        transform.translation.x = mycoords.0.x;
+        transform.translation.y = mycoords.0.y;
+    }
 }
